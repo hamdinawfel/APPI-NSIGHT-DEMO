fe0210fd84806f0098a23e647f321ba99599c54f
Nawfel Hamdi <nawfel.hamdi@tn.ey.com>
2025-04-08 23:58:47 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APPI_NSIGHT_DEMO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
